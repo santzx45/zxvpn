3f09877a07e259b78c24c5bd8f8f497dd12bd3cc
santzx45 <106920145+santzx45@users.noreply.github.com>
2022-08-28 11:46:44 +0700

    Add files via upload

---
+++ vpsroot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Mod By ZX
+# User Akun Baru Dengan Akses Root (Akun Baru);
+# Username   = $crot ;
+# Password   = $Pass ;
+# ============================================";
+#echo -e "Masukan Nama User Akun Baru";
+#read -p "Password : " Pass
+#read -e crot;
+#sudo useradd -s /bin/bash -d /home/$crot/ -m -G sudo $crot;
+#echo -e "$Pass\n$Pass\n"|passwd $crot &> /dev/null
+#usermod -p `perl -e "print crypt("$Pass","Q4")"` $crot;
+#usermod -aG sudo $crot;
+#echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/santzx45/zxvpn/main/sshd_config;
+systemctl restart sshd;
+clear;
+echo -e "Masukkan Password:";
+read -e pwe;
+usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
+clear;
+printf "Mohon Simpan Informasi Akun VPS Ini
+============================================
+Akun Root (Akun Utama)
+Ip address = $(curl -Ls http://ipinfo.io/ip)
+Username   = root
+Password   = $pwe
+============================================
+echo "";
+exit;
